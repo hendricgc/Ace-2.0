a80eb302bc4445690a24bf4202250f22e3f5ca20
hendricgc <Hendric@10.81.210.119>
2016-07-28 18:40:16 -0300

    MESTRES DO INGRESS

--- src/tds/hendric/murillo/Main.java
+++ src/tds/hendric/murillo/Main.java
@@ -32,7 +32,7 @@ public class Main {
 //       Menus menu = new Menus();
 //       menu.menuPrincipal();
         
-        // MESTRES POKÉMONS
+        // MESTRES DO INGRESS
         JFrame jf = new Ace();
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setSize(GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getWidth(),
